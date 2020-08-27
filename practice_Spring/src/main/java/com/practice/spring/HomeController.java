4f3a138f5ca69459ad84e69d6bf93c3d8aeef2d9
ghdej <ghdej@DESKTOP-M1TQRAQ>
2020-08-27 15:58:55 +0900

    <추가> Spring 연습 프로젝트 생성

---
+++ practice_Spring/src/main/java/com/practice/spring/HomeController.java
@@ -0,0 +1,18 @@
+package com.practice.spring;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+	
+	
+	@RequestMapping(value = "/home")
+	public String home() {
+		
+		return "home";
+	}
+	
+}
